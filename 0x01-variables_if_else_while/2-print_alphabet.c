6e4c0dfbc84034c3f8e59d38d394bdbeba78e557
biruk1122 <birukfikadu29@gmail.com>
2023-06-16 05:36:07 -0700

    2 print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -17,26 +17,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-
-3-print_alphabets.c
-
-#include <stdio.h>
-
-/**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
-
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
-}
