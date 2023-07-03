68992a2663557e0434370629cc3af2247c355e9e
biruk1122 <birukfikadu29@gmail.com>
2023-07-03 04:55:09 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - The Entry point
+ * @haystack: The input Value 
+ * @needle: The input value
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+
+		if (*b == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
