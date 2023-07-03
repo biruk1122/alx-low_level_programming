14c5b1c13774ecb5dc330b13ef04967aeb77acfd
biruk1122 <birukfikadu29@gmail.com>
2023-07-03 05:06:57 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,14 +4,14 @@
  * @a: The array Value
  * Return: Always 0 (Success)
  */
-void print_chessboard(char (*c)[10])
+void print_chessboard(char (*c)[8])
 {
 	int a;
 	int b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 8; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b < 8; b++)
 			_putchar(c[a][b]);
 		_putchar('\n');
 	}
