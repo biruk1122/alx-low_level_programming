7c24dfd85d9c6aef7569225cd44267fb6cb1c515
biruk1122 <birukfikadu29@gmail.com>
2023-07-03 04:12:55 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-memcpy.c -o 1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memcpy - The function that copies memory data to the area
+ *@dest: The Memory Location where it is stored
+ *@src: The Memory Location where it is copied
+ *@n: The Number of bytes
+ *
+ *Return: The Copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a = 0;
+	int b = n;
+
+	for (; a < s; a++)
+	{
+		dest[a] = src[a];
+		n--;
+	}
+	return (dest);
+}
