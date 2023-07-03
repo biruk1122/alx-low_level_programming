518c66a9a3bd9fd84568e3fc3458a9f3cc12e451
biruk1122 <birukfikadu29@gmail.com>
2023-07-03 04:03:34 -0700

    pointer to pointer

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putchar -To Write the character c to the stdout
+ * _putchar - To Write the character c to the stdout
  * @c: Print The character to print the OutPut
  *
  * Return: On the success 1.
- * On error,The vale of -1 is returned, and An errno is set to the appropriate Value.
+ * On error, The vale of -1 is returned, and An errno is set to the appropriate Value.
  */
 int _putchar(char c)
 {
